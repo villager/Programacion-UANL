f721e335a3d0ae9d635bb91b66bb52dbaa3c7499
Bema Bemel <aldair.beltran@outlook.com>
2024-09-20 21:09:35 -0600

    Merge some repos

---
+++ Programacion-Avanzada-FCFM/resource.h
@@ -0,0 +1,76 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Project1.rc
+//
+#define IDD_ADD_ITEM                    101
+#define IDD_MAIN                        105
+#define IDD_LOGIN                       107
+#define IDD_LOGIN_MAIN                  107
+#define IDR_MENU1                       110
+#define IDD_ABOUT                       111
+#define IDD_DELETE                      113
+#define IDD_DIALOG1                     132
+#define IDD_DIALOG2                     134
+#define IDC_DATETIMEPICKER1             1004
+#define IDC_COMBO1                      1010
+#define IDC_MFCCOLORBUTTON1             1022
+#define TXT_NAME                        1023
+#define TXT_DATE                        1024
+#define TXT_SPECIE                      1025
+#define TXT_GENDER                      1026
+#define IDC_NAME                        1028
+#define IDC_PHONE                       1029
+#define IDC_PET_NAME                    1030
+#define BN_OK_ADD                       1031
+#define RADIX_IS_MALE                   1032
+#define RADIX_IS_FEMALE                 1035
+#define TEXT_TIME                       1037
+#define IDC_EDIT1                       1038
+#define IDC_PRE_DELETE                  1038
+#define IDC_PASSWORD                    1039
+#define BN_LOGIN                        1040
+#define IDC_USERNAME                    1041
+#define IDC_LIST1                       1059
+#define FIRST_PIC                       1060
+#define IDC_COMBO2                      1061
+#define IDC_DATETIMEPICKER2             1062
+#define TXT_COUNT                       1064
+#define IDC_DENY                        1065
+#define IDC_BUTTON1                     1067
+#define IDC_BUTTON2                     1068
+#define IDC_ANIMATE1                    1069
+#define IDC_LIST_FULLY                  1072
+#define TXT_VET_NAME                    1073
+#define TXT_INFO_DATA                   1074
+#define TXT_VET_NAME2                   1074
+#define IDC_TAB1                        1075
+#define TXT_VET_NAME3                   1075
+#define IDC_LIST_DELETE                 1076
+#define BN_DOC_PIC                      1077
+#define DOC_PIC_2                       1080
+#define PIC_ADD_PET                     1081
+#define ID_CONFIGURACIONES_LENGUAJE     40005
+#define ID_CONFIGURACIONES_ID           40006
+#define ID_CITAS_EDITAR                 40009
+#define ID_CITAS_LISTA                  40011
+#define ID_SALIR                        40014
+#define ID_INFORMACION                  40016
+#define ID_LENGUAJE_ESPA40017           40017
+#define ID_LENGUAJE_INGLES              40018
+#define ID_CITAS_AGREGAR                40019
+#define ID_CITAS_EDITAR40020            40020
+#define ID_CITAS_ELIMINAR               40021
+#define ID_CITAS_PAGAR                  40022
+#define ID_CITAS_LISTADO                40023
+#define MENU_HOME                       40024
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         40025
+#define _APS_NEXT_CONTROL_VALUE         1082
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
